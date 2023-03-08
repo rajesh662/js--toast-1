d4baba97fe42c7ad13ff74dea657852dea528ee8
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-03-08 14:53:44 +0530

    upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function on1(){
+    let cont = document.querySelector(".container");
+    cont.className="rk";
+
+    setTimeout(function(){
+        // cont.className=cont.className.replace("rk","rk1");
+        let rajesh = document.querySelector(".contai");
+       rajesh.style.display="none";
+    },4000);
+
+}
+
+
+
+    
